7ec94f9ba1f2903feffe9eff391f332451178e1b
lumidee007 <oluwafemi.ayinla@gmail.com>
2021-08-18 04:22:38 +0100

    added footer component

---
+++ src/component/footer/styles/footer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FooterContainer= styled.div `
+    display: flex;
+    justify-content: space-between;
+    margin: 40px 6px 0;
+    font-size: 20px;
+`
+
